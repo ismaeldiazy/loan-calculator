167773d78a1ccf8a431428d18acacc1511d5ae75
ismaeldiazy <ismaeldiazy@gmail.com>
2020-04-01 01:17:10 +0200

    Created React development environment, initialized the React App with a hi world component, and added custom CSS.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+import Header from './components/Header.js';
+function App() {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
+}
+
+export default App;
